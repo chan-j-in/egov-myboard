ca1420333a6e0cd49e1a68e590865011972fe272
l__o_ <genius8111@naver.com>
2025-02-14 23:22:02 +0900

    feat:select 추가

--- myboard/src/main/java/main/service/BoardService.java
+++ myboard/src/main/java/main/service/BoardService.java
@@ -1,6 +1,9 @@
 package main.service;
 
+import java.util.List;
+
 public interface BoardService {
 
 	public String insertNBoard(BoardVO vo) throws Exception;
+	public List<?> selectNBoardList(BoardVO vo) throws Exception;
 }
