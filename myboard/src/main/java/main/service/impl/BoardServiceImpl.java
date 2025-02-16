b1080664bd36d4aac808b3ecbceffe118f74ef65
l__o_ <genius8111@naver.com>
2025-02-16 16:22:12 +0900

    fix:select 페이징 추가

--- myboard/src/main/java/main/service/impl/BoardServiceImpl.java
+++ myboard/src/main/java/main/service/impl/BoardServiceImpl.java
@@ -1,6 +1,8 @@
 package main.service.impl;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 
@@ -22,9 +24,36 @@ public class BoardServiceImpl implements BoardService {
 	}
 
 	@Override
-	public List<?> selectNBoardList(BoardVO vo) throws Exception {
+	public Map<String, Object> selectNBoardList(int page, BoardVO vo) throws Exception {
 		
-		return boardDAO.selectNBoardList(vo);
+		int total = selectNBoardTotal(vo);
+		int totalPage = (int) Math.ceil(total / 10.0);
+		
+		if(page < 1 || page > totalPage) page = 1;
+		
+		int pageSize = 10;
+		int startIndex = (page - 1) * pageSize + 1;
+		int endIndex = page * pageSize;
+		
+		vo.setStartIndex(startIndex);
+		vo.setEndIndex(endIndex);
+		
+		List<?> resultList=boardDAO.selectNBoardList(vo);
+		
+		Map<String, Object> resultMap = new HashMap<>();
+		resultMap.put("total", total);
+		resultMap.put("totalPage", totalPage);
+		resultMap.put("resultList", resultList);
+		resultMap.put("startIndex", startIndex);
+		resultMap.put("endIndex", endIndex);
+		
+		return resultMap;
+	}
+
+	@Override
+	public int selectNBoardTotal(BoardVO vo) throws Exception {
+		
+		return boardDAO.selectNBoardTotal(vo);
 	}
 
 }
