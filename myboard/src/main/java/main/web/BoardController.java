5b218bb35897ccfc024055d8ff38aec8063342e3
l__o_ <genius8111@naver.com>
2025-02-17 17:25:15 +0900

    feat: 게시글 상세 구현

--- myboard/src/main/java/main/web/BoardController.java
+++ myboard/src/main/java/main/web/BoardController.java
@@ -6,6 +6,7 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -40,5 +41,12 @@ public class BoardController {
 		
 		model.addAllAttributes(data);
 		return "board/boardList";
+	}
+	
+	@RequestMapping("/boardDetail.do")
+	public String selectNBoardDetail(int unq, ModelMap model) throws Exception {
+		
+		model.addAttribute(boardService.selectNBoardDetail(unq));
+		return "board/boardDetail";
 	}
 }
