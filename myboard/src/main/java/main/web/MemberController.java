516d5d0d7e9b55657e5c82f71d8dd2fec678c9da
l__o_ <genius8111@naver.com>
2025-02-24 21:23:11 +0900

    feat: 우편번호 입력 메소드 추가, 로그인/로그아웃 구현

--- myboard/src/main/java/main/web/MemberController.java
+++ myboard/src/main/java/main/web/MemberController.java
@@ -1,8 +1,12 @@
 package main.web;
 
+import java.util.List;
+
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -34,5 +38,53 @@ public class MemberController {
 	public String idCheck(String userid) throws Exception {
 		
 		return memberService.selectMemberIdCheck(userid)+"";
+	}
+	
+	@RequestMapping("/post1.do")
+	public String post1() {
+		
+		return "member/post1";
+	}
+
+	@RequestMapping("/post2.do")
+	public String post2(String dong, ModelMap model) throws Exception {
+		
+		List<?> resultList = memberService.selectPostList(dong);
+		
+		if(resultList == null || resultList.isEmpty()) {
+			model.addAttribute("errorMsg", "검색된 주소가 없습니다.");
+			return "member/post1";
+		}
+		model.addAttribute("resultList", resultList);
+		return "member/post2";
+	}
+	
+	@RequestMapping("/loginWrite.do") 
+	public String loginWrite() {
+		
+		return "member/loginWrite";
+	}
+	
+	@RequestMapping("/loginWriteSub.do")
+	@ResponseBody
+	public String selectMemberCount(MemberVO vo, HttpSession session) throws Exception {
+		
+		if(memberService.selectMemberIdCheck(vo.getUserid())==0) return "-1";
+
+		if(memberService.selectMemberCount(vo)==1) {
+			session.setAttribute("SessionUserId", vo.getUserid());
+			System.out.println("result : ok");
+			return "ok";
+		} else {
+			System.out.println("result : fail");
+			return "fail";
+		}
+	}
+	
+	@RequestMapping("/logout.do")
+	public String logout(HttpSession session) {
+		
+		session.removeAttribute("SessionUserId");
+		return "member/loginWrite";
 	}
 }
