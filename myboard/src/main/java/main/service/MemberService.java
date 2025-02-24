da8eff0206fd09839e2cdccd89c720a0d447083d
l__o_ <genius8111@naver.com>
2025-02-24 21:23:46 +0900

    feat: 우편번호 찾기, 아이디/비밀번호 찾기 sql 추가

--- myboard/src/main/java/main/service/MemberService.java
+++ myboard/src/main/java/main/service/MemberService.java
@@ -1,7 +1,11 @@
 package main.service;
 
+import java.util.List;
+
 public interface MemberService {
 
 	public String insertMember(MemberVO vo) throws Exception;
 	public int selectMemberIdCheck(String userid) throws Exception;
+	public List<?> selectPostList(String dong) throws Exception;
+	public int selectMemberCount(MemberVO vo) throws Exception;
 }
